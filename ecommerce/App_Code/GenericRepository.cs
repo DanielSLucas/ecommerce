19a5155d72d9b3f8de02518f6fbaf79519b6c028
Daniel Lucas Pessoa Monteiro dos Santos <daniellucas-pms@hotmail.com>
2023-03-22 17:54:36 -0300

    feat: GenericRepository & sqlService

---
+++ ecommerce/App_Code/GenericRepository.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.IdentityModel.Metadata;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using System.Web.Services;
+
+/// <summary>
+/// Descrição resumida de Repository
+/// </summary>
+public class GenericRepository<T>
+{
+    private List<String> entityColumns = new List<String>();
+    private String entityName;
+    private SqlService sqlService;
+
+    public GenericRepository(String entityName, SqlService sqlService)
+    {
+        mapEntityColumns();
+        this.entityName = entityName;
+        this.sqlService = sqlService;
+    }
+
+    private void mapEntityColumns()
+    {
+        foreach (var prop in typeof(T).GetProperties())
+        {
+            entityColumns.Add(prop.Name);
+        }
+    }
+
+    public DataSet listAll()
+    {
+        String SQL = String.Format("SELECT * FROM {0}", entityName);
+        return sqlService.query(SQL);
+    }
+
+    public DataSet findById(int id)
+    {
+        String SQL = String.Format("SELECT * FROM {0} WHERE id={1}", entityName, id);
+        return sqlService.query(SQL);
+    }
+
+    public String create(T entity)
+    {
+        String columns = String.Join(", ", entityColumns);
+        List<object> valuesList = new List<object>();
+
+        foreach (var column in entityColumns)
+        {
+            object columnValue = typeof(T).GetProperty(column).GetValue(entity);
+            String columntype = columnValue.GetType().ToString();
+
+            if (columntype == "System.String" || columntype == "System.DateTime")
+            {
+                valuesList.Add("'" + columnValue + "'");
+            }
+            else
+            {
+                valuesList.Add(columnValue);
+            }
+        }
+
+        String values = String.Join(", ", valuesList);
+
+        String SQL = String.Format(
+            "INSERT INTO {0} ({1}) values ({2})",
+            entityName, columns, values
+        );
+
+        sqlService.nonQuery(SQL);
+
+        return "Registro adicionado com sucesso";
+    }
+
+    public String update(int id, T entity)
+    {
+        List<String> valuesList = new List<String>();
+
+        foreach (var column in entityColumns)
+        {
+            object columnValue = typeof(T).GetProperty(column).GetValue(entity);
+            String columntype = columnValue.GetType().ToString();
+
+            if (columntype == "System.String" || columntype == "System.DateTime")
+            {
+                valuesList.Add(
+                    String.Format("{0}={1}", column, "'" + columnValue + "'")
+                );
+            }
+            else
+            {
+                valuesList.Add(
+                    String.Format("{0}={1}", column, columnValue)
+                );
+            }
+        }
+
+        String values = String.Join(", ", valuesList);
+
+        String SQL = String.Format(
+            "UPDATE {0} SET {1} WHERE id={2}",
+            entityName, values, id
+        );
+
+        sqlService.nonQuery(SQL);
+
+        return "Registro atualizado com sucesso";
+    }
+
+    public String delete(int id)
+    {
+        String SQL = String.Format(
+            "DELETE FROM {0} WHERE id={1}",
+            entityName, id
+        );
+
+        sqlService.nonQuery(SQL);
+
+        return "Registro deletado com sucesso";
+    }
+}
---
+++ ecommerce/App_Code/SqlService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+
+/// <summary>
+/// Descrição resumida de SqlService
+/// </summary>
+public class SqlService
+{
+    private SqlConnection con;
+    private static SqlService intance = null;
+
+    public SqlService(String connectionString)
+    {
+        con = new SqlConnection(connectionString);
+    }
+
+    public static SqlService getInstance(String connectionString)
+    {
+        if(getInstance == null)
+        {
+            intance = new SqlService(connectionString);
+        }
+
+        return intance;
+    }
+
+    public DataSet query(String SQL)
+    {
+        DataSet ds = new DataSet();
+        SqlDataAdapter dataAdapter = new SqlDataAdapter();
+
+        dataAdapter.SelectCommand = new SqlCommand(SQL, con);
+        dataAdapter.Fill(ds);
+
+        return ds;
+    }
+
+    public void nonQuery(String SQL)
+    {
+        SqlCommand cmd = new SqlCommand(SQL, con);
+
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+    }
+
+}
